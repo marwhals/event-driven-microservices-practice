5ca8e076a160581c22e118c0dd6bda23c5339a2a
marwhals <marjan.mubarok2012@gmail.com>
2025-12-12 10:07:48 +0000

    Setup root pom

---
+++ src/test/java/microservices/practice/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
