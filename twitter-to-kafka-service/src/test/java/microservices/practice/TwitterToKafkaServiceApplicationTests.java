9058a99c70614dab0bc5a388d6f718b40b341a76
marwhals <marjan.mubarok2012@gmail.com>
2025-12-13 07:40:15 +0000

    Setup microservice

--- twitter-to-kafka-service/src/test/java/microservices/practice/TwitterToKafkaServiceApplicationTests.java
+++ twitter-to-kafka-service/src/test/java/microservices/practice/TwitterToKafkaServiceApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroservicesDemoApplicationTests {
+public class TwitterToKafkaServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void contextLoad() {
 
+    }
 }
