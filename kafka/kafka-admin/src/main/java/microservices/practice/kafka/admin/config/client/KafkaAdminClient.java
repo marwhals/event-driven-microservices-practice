d482a8f5029e3206498e1d2b9b6aabf7fbe38aa8
marwhals <marjan.mubarok2012@gmail.com>
2025-12-21 16:51:55 +0000

    Setup Configuration and dependencies for kafka-admin module

---
+++ kafka/kafka-admin/src/main/java/microservices/practice/kafka/admin/config/client/KafkaAdminClient.java
@@ -0,0 +1,33 @@
+package microservices.practice.kafka.admin.config.client;
+
+import microservices.practice.demo.config.KafkaConfigData;
+import microservices.practice.demo.config.RetryConfigData;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaAdminClient {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaAdminClient.class);
+
+    private final KafkaConfigData kafkaConfigData;
+
+    private final RetryConfigData retryConfigData;
+
+    private final AdminClient adminClient;
+
+    private final RetryTemplate retryTemplate;
+
+    public KafkaAdminClient(KafkaConfigData kafkaConfigData,
+                            RetryConfigData retryConfigData,
+                            AdminClient adminClient,
+                            RetryTemplate retryTemplate) {
+        this.kafkaConfigData = kafkaConfigData;
+        this.retryConfigData = retryConfigData;
+        this.adminClient = adminClient;
+        this.retryTemplate = retryTemplate;
+    }
+}
--- kafka/notes.md
+++ kafka/notes.md
@@ -7,3 +7,7 @@
 - kafka-model - Create and hold Java Objects for Kafka in Avro format
 - kafka-admin - Create and verify Kafka topics programmatically
 - kafka-producer - Use spring-kafka to write Kafka producer implementation
+
+---
+
+- Spring Retry - Automatically retry an operation
