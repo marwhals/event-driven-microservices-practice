a6d2c60f5740b5f933f1b6d5a721d0af088da54c
marwhals <marjan.mubarok2012@gmail.com>
2025-12-21 19:44:06 +0000

    Implement automatic kafka topic creation

---
+++ kafka/kafka-admin/src/main/java/microservices/practice/kafka/admin/exception/KafkaClientException.java
@@ -0,0 +1,15 @@
+package microservices.practice.kafka.admin.exception;
+
+public class KafkaClientException extends RuntimeException {
+
+    public KafkaClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public KafkaClientException(String message) {
+        super(message);
+    }
+
+    public KafkaClientException() {
+    }
+}
