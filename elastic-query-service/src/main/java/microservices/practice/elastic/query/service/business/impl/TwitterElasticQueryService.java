a729aeff406c6979a8632b79c3bd7d1cd2cb4bba
marwhals <marjan.mubarok2012@gmail.com>
2026-04-03 20:31:14 +0100

    Add hateoas capability to Rest API with Spring Hateoas

--- elastic-query-service/src/main/java/microservices/practice/elastic/query/service/business/impl/TwitterElasticQueryService.java
+++ elastic-query-service/src/main/java/microservices/practice/elastic/query/service/business/impl/TwitterElasticQueryService.java
@@ -4,6 +4,7 @@ import elastic.model.index.impl.TwitterIndexModel;
 import microservices.practice.elastic.query.service.ElasticQueryClient;
 import microservices.practice.elastic.query.service.business.ElasticQueryService;
 import microservices.practice.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import microservices.practice.elastic.query.service.model.assembler.ElasticQueryServiceResponseModelAssembler;
 import microservices.practice.elastic.query.service.transformer.ElasticToResponseModelTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +15,13 @@ public class TwitterElasticQueryService implements ElasticQueryService {
 
     private static final Logger LOG = LoggerFactory.getLogger(TwitterElasticQueryService.class);
 
-    private final ElasticToResponseModelTransformer elasticToResponseModelTransformer;
+    private final ElasticQueryServiceResponseModelAssembler elasticQueryServiceResponseModelAssembler;
 
     private final ElasticQueryClient<TwitterIndexModel> elasticQueryClient;
 
-    public TwitterElasticQueryService(ElasticToResponseModelTransformer elasticToResponseModelTransformer, ElasticQueryClient<TwitterIndexModel> elasticQueryClient) {
-        this.elasticToResponseModelTransformer = elasticToResponseModelTransformer;
+    public TwitterElasticQueryService(ElasticQueryServiceResponseModelAssembler assembler,
+                                      ElasticQueryClient<TwitterIndexModel> elasticQueryClient) {
+        this.elasticQueryServiceResponseModelAssembler = assembler;
         this.elasticQueryClient = elasticQueryClient;
     }
 
@@ -27,18 +29,18 @@ public class TwitterElasticQueryService implements ElasticQueryService {
     @Override
     public ElasticQueryServiceResponseModel getDocumentById(String id) {
         LOG.info("Querying elasticsearch by id {}", id);
-        return elasticToResponseModelTransformer.getResponseModel(elasticQueryClient.getIndexModelById(id));
+        return elasticQueryServiceResponseModelAssembler.toModel(elasticQueryClient.getIndexModelById(id));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getDocumentByText(String text) {
         LOG.info("Querying elasticsearch by id {}", text);
-        return elasticToResponseModelTransformer.getResponseModels(elasticQueryClient.getIndexModelByText(text));
+        return elasticQueryServiceResponseModelAssembler.toModels(elasticQueryClient.getIndexModelByText(text));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getAllDocuments() {
         LOG.info("Querying all documents in elasticsearch");
-        return elasticToResponseModelTransformer.getResponseModels(elasticQueryClient.getAllIndexModels());
+        return elasticQueryServiceResponseModelAssembler.toModels(elasticQueryClient.getAllIndexModels());
     }
 }
