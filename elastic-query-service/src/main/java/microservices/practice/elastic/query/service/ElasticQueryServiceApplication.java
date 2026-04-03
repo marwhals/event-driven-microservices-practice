2099f1e896d8ba5eb03c523f64f08888c84cfd1e
marwhals <marjan.mubarok2012@gmail.com>
2026-04-03 17:17:26 +0100

    Add elastic query microservice

---
+++ elastic-query-service/src/main/java/microservices/practice/elastic/query/service/ElasticQueryServiceApplication.java
@@ -0,0 +1,15 @@
+package microservices.practice.elastic.query.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "microservices.practice")
+public class ElasticQueryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticQueryServiceApplication.class, args);
+    }
+
+
+}
---
+++ elastic-query-service/src/test/java/microservices/practice/elastic/query/service/ElasticQueryServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package microservices.practice.elastic.query.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ElasticQueryServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
