23307b0caae1f310139b85943d0ac593985e74d3
marwhals <marjan.mubarok2012@gmail.com>
2026-04-15 21:01:30 +0100

    Start to implement elastic-web-query-web-client

---
+++ elastic-query-web-client/src/test/java/microservices/practice/ElasticQueryWebClientApplicationTests.java
@@ -0,0 +1,14 @@
+package microservices.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ElasticQueryWebClientApplicationTests {
+
+    @Test
+    public void contextLoad() {
+
+    }
+
+}
