ceed5df9afe28f69e32275fb2c7f952f26af95ba
marwhals <marjan.mubarok2012@gmail.com>
2026-04-16 21:32:34 +0100

    Creating Rest API

---
+++ elastic-query-web-client/src/main/java/microservices/practice/model/ElasticQueryWebClientRequestModel.java
@@ -0,0 +1,21 @@
+package microservices.practice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ElasticQueryWebClientRequestModel {
+
+    private String id;
+    @NotEmpty
+    private String text;
+
+
+}
---
+++ elastic-query-web-client/src/main/java/microservices/practice/model/ElasticQueryWebClientResponseModel.java
@@ -0,0 +1,21 @@
+package microservices.practice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ElasticQueryWebClientResponseModel {
+
+    private String id;
+    private Long userId;
+    private String text;
+    private LocalDateTime createdAt;
+
+}
